fa2319da8d4c3466176a757475f884d009e04f88
Orhan Isik <isiko979@gmail.com>
2023-07-03 14:17:16 +0300

    problem class created on master

---
+++ src/login/Problem.java
@@ -0,0 +1,16 @@
+package login;
+
+public class Problem {
+
+    public static void main(String[] args) {
+
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
+
+        }
+
+        //some code here master
+        //another code here master
+    }
+}
